208801495c055d82ad82bf0c4df8e18c34e87832
AtiyaNova <atiyanova@gmail.com>
2021-12-21 19:42:12 -0500

    Scenario 1 fleshed out

--- Unity/PathOS/Assets/PathOS+/Scripts/Agents/PathOSAgent.cs
+++ Unity/PathOS/Assets/PathOS+/Scripts/Agents/PathOSAgent.cs
@@ -325,8 +325,6 @@ public class PathOSAgent : MonoBehaviour
 
             newAdrenaline = 0.5f;
             modifiableHeuristicScales[adrenalineIndex].scale = newAdrenaline;
-
-            Debug.Log("ratata" + newCaution);
         }
         else
         {
